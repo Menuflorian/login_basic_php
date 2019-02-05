6d85d48404d18aa910a95f80f53ea4794482337c
Menu Florian <40692712+Menuflorian@users.noreply.github.com>
2019-02-05 21:39:24 +0100

    correction bug

--- confirm.php
+++ confirm.php
@@ -7,12 +7,12 @@ $token = $_GET['token'];
 $req = $pdo -> prepare('SELECT confirmation_token FROM users WHERE id = ?');
 $req -> execute([$user_id]);
 $user = $req -> fetch();
-    session_start();
+session_start();
 if($user && $user->confirmation_token == $token){
     $pdo->prepare('UPDATE users SET confirmation_token = NULL, Confirmed_at = NOW() WHERE id= ?')->execute([$user_id]);
     $req = $pdo -> prepare('SELECT * FROM users WHERE id = ?');
     $req -> execute([$user_id]);
-    $user = $req -> fetchall();
+    $user = $req -> fetch();
     $_SESSION['auth'] = $user;
     $_SESSION['flash']['success'] = 'Votre compte a bien été validé.';
     header('Location: account.php');
